10bf21a72cef240d2332453ee98d8184737580d0
Inqiyad <inqiyad18@gmail.com>
2022-07-21 23:46:26 -0400

    Styling and controller

--- controllers/userinfo.js
+++ controllers/userinfo.js
@@ -1,77 +1,108 @@
-const StudentDetails = require('../models/student')
-const TutorDetails = require('../models/tutor')
-const admin = require("firebase-admin")
+const StudentDetails = require("../models/student");
+const TutorDetails = require("../models/tutor");
+const admin = require("firebase-admin");
 
-const userInfo = async(req, res) => {
-    let {first_name, last_name, number, role} = await req.body
-    if (role == 'student') {
-        await StudentDetails.create({firstname: first_name, lastname: last_name, phone: number})
-    } else if (role == 'tutor') {
-        await TutorDetails.create({firstname: first_name, lastname: last_name, phone: number})
-    }
-    res.json({
-        success: 'Success'
-    })
-}
+const userInfo = async (req, res) => {
+  let { first_name, last_name, number, role } = await req.body;
+  if (role == "student") {
+    await StudentDetails.create({
+      firstname: first_name,
+      lastname: last_name,
+      phone: number,
+    });
+  } else if (role == "tutor") {
+    await TutorDetails.create({
+      firstname: first_name,
+      lastname: last_name,
+      phone: number,
+    });
+  }
+  res.json({
+    success: "Success",
+  });
+};
 
-const postingInfo = async(req, res) => {
-    const body = req.body
-    const number = body.phone_number
-    await StudentDetails.updateOne({phone: number}, {$push:
-        {   posts: {
-                email: body.email,
-                medium: body.medium,
-                subjects: body.subject,
-                location: body.location,
-                class: body.class,
-                presence: body.online,
-                max_salary: body.max_salary,
-                min_salary: body.min_salary,
-                tutor_gender: body.preferred_gender, 
-                student_gender: body.student_gender,
-                availability_days: body.days,
-                preferred_institution: body.school
-            }
+const postingInfo = async (req, res) => {
+  const body = req.body;
+  const number = body.phone_number;
+  await StudentDetails.updateOne(
+    { phone: number },
+    {
+      $push: {
+        posts: {
+          email: body.email,
+          medium: body.medium,
+          subjects: body.subject,
+          location: body.location,
+          class: body.class,
+          presence: body.online,
+          max_salary: body.max_salary,
+          min_salary: body.min_salary,
+          tutor_gender: body.preferred_gender,
+          student_gender: body.student_gender,
+          availability_days: body.days,
+          preferred_institution: body.school,
         },
-    })
-    res.json({
-        success: 'Success'
-    })
-}
+      },
+    }
+  );
+  res.json({
+    success: "Success",
+  });
+};
 
-const getPostInfo = async(req, res) => {
-    const data = await StudentDetails.find({})
-    //console.log(data)
-    res.json({
-        data
-    })
-}
+const getPostInfo = async (req, res) => {
+  const data = await StudentDetails.find({});
+  //console.log(data)
+  res.json({
+    data,
+  });
+};
 
-const tutorlist = async(req, res) => {
-    const data = await TutorDetails.find({})
-    res.json({
-        data
-    })
-}
+const tutorlist = async (req, res) => {
+  const data = await TutorDetails.find({});
+  res.json({
+    data,
+  });
+};
 
-const applied = async(req, res) => {
-    const token = req.body.token
-    const phone = await verify(token)
-    const user = await TutorDetails.find({phone: phone})
-    const id = user[0]._id
-    
-}
+const applied = async (req, res) => {
+  const token = req.body.token;
+  const phone = await verify(token);
+  const user = await TutorDetails.find({ phone: phone });
+  const id = user[0]._id;
+};
+
+const getUserPosts = async (req, res) => {
+  const authorization = req.headers.authorization;
+  const token = authorization.split(" ")[1];
+  const phone = await verify(token);
+  const user = await StudentDetails.find({ phone: phone });
+  res.json({
+    user
+  });
+};
 
 async function verify(token) {
-    let phone = ''
-    await admin.auth().verifyIdToken(token)
+  let phone = "";
+  await admin
+    .auth()
+    .verifyIdToken(token)
     .then((decodedToken) => {
-        const uid = decodedToken.uid
-        phone = decodedToken.phone_number
-    }).catch((error) => {
-        console.log(error)
+      const uid = decodedToken.uid;
+      phone = decodedToken.phone_number;
     })
-    return phone
+    .catch((error) => {
+      console.log(error);
+    });
+  return phone;
 }
 
-module.exports = {userInfo, postingInfo, getPostInfo, tutorlist, applied}
+module.exports = {
+  userInfo,
+  postingInfo,
+  getPostInfo,
+  tutorlist,
+  applied,
+  getUserPosts,
+};
--- routes/userinfo.js
+++ routes/userinfo.js
@@ -1,12 +1,20 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const{userInfo, postingInfo, getPostInfo, tutorlist, applied} = require('../controllers/userinfo')
+const {
+  userInfo,
+  postingInfo,
+  getPostInfo,
+  tutorlist,
+  applied,
+  getUserPosts,
+} = require("../controllers/userinfo");
 
-router.post('/info', userInfo)
-router.route('/posting').post(postingInfo).get(getPostInfo)
-router.get('/tutors', tutorlist)
-router.post('/apply', applied)
+router.post("/info", userInfo);
+router.route("/posting").post(postingInfo).get(getPostInfo);
+router.get("/tutors", tutorlist);
+router.post("/apply", applied);
+router.get("/myposts", getUserPosts);
 // router.post('/posting', postingInfo)
 
-module.exports = router
+module.exports = router;
