5178a1505f1ed603528d851b7d996b5f1250917d
Inqiyad <inqiyad18@gmail.com>
2022-08-01 00:31:13 -0400

    Institution

--- models/student.js
+++ models/student.js
@@ -1,104 +1,112 @@
 // Student/Parent Details
-const mongoose = require('mongoose')
-const validator = require('validator')
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const validator = require("validator");
+const Schema = mongoose.Schema;
 
 const StudentSchema = new mongoose.Schema({
-    firstname: {
+  firstname: {
+    type: String,
+    required: [true, "Please provide your first name"],
+    maxlength: 50,
+    minlength: 3,
+  },
+  lastname: {
+    type: String,
+    maxlength: 50,
+    minlength: 3,
+  },
+  email: {
+    type: String,
+    //required: [true, 'Please provide your email'],
+    // match: [
+    // /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    // 'Please provide a valid email',
+    // ],
+    //unique: true,
+    //validate: [validator.isEmail, 'Please provide a valid email']
+  },
+  phone: {
+    type: String,
+    required: [true, "Please provide a valid phone number"],
+    maxlength: 20,
+    unique: true,
+  },
+  // major: {
+  //     type: String,
+  //     minlength: 3
+  // },
+  posts: [
+    {
+      institution: {
         type: String,
-        required: [true, 'Please provide your first name'],
-        maxlength: 50,
-        minlength: 3,
-    },
-    lastname: {
+        minlength: 5,
+      },
+      firstname: {
         type: String,
-        maxlength: 50,
-        minlength: 3,
-    },
-    email: {
+      },
+      lastname: {
         type: String,
-        //required: [true, 'Please provide your email'],
-        // match: [
-        // /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-        // 'Please provide a valid email',
-        // ],
-        //unique: true,
-        //validate: [validator.isEmail, 'Please provide a valid email']
-    },
-    phone: {
+      },
+      date: {
         type: String,
-        required: [true, 'Please provide a valid phone number'],
-        maxlength: 20,
-        unique: true
-    },
-    // major: {
-    //     type: String,
-    //     minlength: 3
-    // },
-    posts: [
+      },
+      medium: {
+        type: String,
+        minlength: 5,
+      },
+      class: {
+        type: String,
+      },
+      presence: {
+        type: String,
+      },
+      subjects: [
         {
-            preferred_institution: {
-                type: String,
-                minlength: 5
-            },
-            firstname: {
-                type: String,
-            },
-            lastname: {
-                type: String,
-            },
-            date: {
-                type: String,
-            },
-            medium: {
-                type: String,
-                minlength: 5
-            },
-            class: {
-                type: String
-            },
-            presence: {
-                type: String,
-            },
-            subjects: [{
-                type: String,
-                //required: [true, 'Please list the subjects you are willing to teach']
-            }],
-            location: {
-                type: String,
-                //required: [true, 'Please list the locations where you are willing to teach']
-            },
-            applicants: {
-                applied: [{
-                    type: Schema.Types.ObjectId,
-                    ref: 'TutorDetails'
-                }],
-                liked: [{
-                    type: Schema.Types.ObjectId,
-                    ref: 'TutorDetails'
-                }],
-                rejected: [{
-                    type: Schema.Types.ObjectId,
-                    ref: 'TutorDetails'
-                }]
-            },
-            max_salary: {
-                type: String
-            }, 
-            min_salary: {
-                type: String
-            },
-            tutor_gender: {
-                type: String,
-            },
-            student_gender: {
-                type: String
-            },
-            availability_days: {
-                type: String
-            }
-        }
-    ]
-})
+          type: String,
+          //required: [true, 'Please list the subjects you are willing to teach']
+        },
+      ],
+      location: {
+        type: String,
+        //required: [true, 'Please list the locations where you are willing to teach']
+      },
+      applicants: {
+        applied: [
+          {
+            type: Schema.Types.ObjectId,
+            ref: "TutorDetails",
+          },
+        ],
+        liked: [
+          {
+            type: Schema.Types.ObjectId,
+            ref: "TutorDetails",
+          },
+        ],
+        rejected: [
+          {
+            type: Schema.Types.ObjectId,
+            ref: "TutorDetails",
+          },
+        ],
+      },
+      max_salary: {
+        type: String,
+      },
+      min_salary: {
+        type: String,
+      },
+      tutor_gender: {
+        type: String,
+      },
+      student_gender: {
+        type: String,
+      },
+      availability_days: {
+        type: String,
+      },
+    },
+  ],
+});
 
-module.exports = mongoose.model("StudentDetails", StudentSchema)
+module.exports = mongoose.model("StudentDetails", StudentSchema);
